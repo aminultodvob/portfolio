d661bdd0b73071b6a00badd9933e9f72854bb240
Aminul Islam <aminul.todvob@gmail.com>
2026-02-28 10:41:41 +0600

    feat: enable google analytics with react-ga4

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { Analytics } from "@vercel/analytics/react";
+import GoogleAnalytics from "./components/GoogleAnalytics";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import AdminLogin from "./pages/AdminLogin";
@@ -23,6 +24,7 @@ const App = () => (
         <Sonner />
         <Analytics />
         <BrowserRouter>
+          <GoogleAnalytics />
           <AuthProvider>
             <Routes>
               <Route path="/" element={<Index />} />
